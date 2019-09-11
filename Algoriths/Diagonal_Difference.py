bce743c194edf29430de6d661925baefc9a61174
Dennis <denniskamau3@gmail.com>
2019-09-11 20:21:15 +0300

    finish diagonal differnce challenge

--- Algoriths/Diagonal_Difference.py
+++ Algoriths/Diagonal_Difference.py
@@ -15,16 +15,10 @@ import sys
 
 def diagonalDifference(arr):
     # Write your code here
-    n = len(arr)
     left_to_right = 0
     right_to_left = 0
-    for i in range(0,n):
-        for j in range(0,n):
-            if(i == j):
-                left_to_right +=arr[i][j]
-            if(i == n - j - 1):
-                right_to_left += arr[i][j]
-    return abs(left_to_right-right_to_left)
+    for elem in arr:
+        print(elem)
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
