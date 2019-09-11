df85e84c66fad368ca42d6d59bdcf3f341145a94
Dennis <denniskamau3@gmail.com>
2019-09-11 19:28:05 +0300

    finish compare the triplets

---
+++ Algoriths/Compare_the_Triplets.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    comparison = []
+    for i in range(3):
+        if a[i] > b[i]:
+            alice += 1
+        if b[i] > a[i]:
+            bob +=1
+    comparison.append(alice)
+    comparison.append(bob)
+    return comparison
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
